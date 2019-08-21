61ac08716d5fbec99347750b0df5b0075da8c192
Daniel Alabuja <Danielal@HA-SHEM.com>
2019-08-21 15:31:16 +0100

    Feat: DTOs

--- Supermarket.Common/DTOs/CategoryDto.cs
+++ Supermarket.Common/DTOs/CategoryDto.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using Supermarket.Common.DTOs;
+using System.Collections.Generic;
 
 namespace Supermarket.Common.DTOs
 {
     public class CategoryDTO
     {
+        public CategoryDTO()
+        {
+            Products = new List<ProductDTO>();
+        }
         public Guid Id { get; set; }
         public string Name { get; set; }
+
+        public virtual ICollection<ProductDTO> Products { get; set; }
     }
 }
