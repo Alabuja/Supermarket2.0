828a88cb071a93f94e47165a9f092be811223a52
Daniel Alabuja <Danielal@HA-SHEM.com>
2019-08-23 10:26:53 +0100

    Fix: DbContext.cs file changed

--- Supermarket.API/Data/SupermarketDbContext.cs
+++ Supermarket.API/Data/SupermarketDbContext.cs
@@ -15,25 +15,20 @@ namespace Supermarket.API.Data
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            builder.Entity<Category>()
+                    .HasIndex(p => new { p.Id })
+                    .IsUnique();
+
+            builder.Entity<Product>()
+                    .HasIndex(p => new { p.Id })
+                    .IsUnique();
+
+            builder.Entity<Product>()
+                .HasOne(c => c.Category)
+                .WithMany(p => p.Products)
+                .OnDelete(DeleteBehavior.Restrict);
+
             base.OnModelCreating(builder);
-
-            builder.Entity<Category>().ToTable("Categories");
-            builder.Entity<Category>().HasKey(p => p.Id);
-            builder.Entity<Category>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
-            builder.Entity<Category>().Property(p => p.Name).IsRequired().HasMaxLength(30);
-            builder.Entity<Category>().HasMany(p => p.Products).WithOne(p => p.Category).HasForeignKey(p => p.CategoryId);
-            builder.Entity<Category>().HasData
-            (
-                new Category { Id = Guid.Parse("9C2EE22C-5955-42BD-A596-EC7117F45789"), Name = "Electronics" },
-                new Category { Id = Guid.Parse("291C0718-74B7-4844-8C0F-8E8DA4EF2551"), Name = "Merchandise" }
-            );
-
-            builder.Entity<Product>().ToTable("Products");
-            builder.Entity<Product>().HasKey(p => p.Id);
-            builder.Entity<Product>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
-            builder.Entity<Product>().Property(p => p.Name).IsRequired().HasMaxLength(50);
-            builder.Entity<Product>().Property(p => p.QuantityInStock).IsRequired();
-            builder.Entity<Product>().Property(p => p.UnitOfMeasurement).IsRequired();
         }
     }
 }
